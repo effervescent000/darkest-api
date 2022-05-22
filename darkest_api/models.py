2cabe35145cc14b59e7b9a2f020c452911ff6c47
effervescent000 <kislio01@gmail.com>
2022-05-22 10:08:36 -0400

    fleshing out hero model

---
+++ darkest_api/models.py
@@ -0,0 +1,51 @@
+from . import db
+from passlib.hash import pbkdf2_sha256 as hash
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    role = db.Column(db.String(200))
+
+    @staticmethod
+    def hash_password(password):
+        return hash.hash(password)
+
+    def check_password(self, input):
+        return hash.verify(input, self.password)
+
+
+class Hero(db.Model):
+    __tablename__ = "heroes"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    hero_class = db.Column(db.String(20), nullable=False)
+    resolve = db.Column(db.Integer, nullable=False, default=0)
+
+    stats = db.relationship(
+        "Stat", backref="hero", lazy=True, cascade="all, delete-orphan"
+    )
+    abilities = db.relationship(
+        "Ability", backref="hero", lazy=True, cascade="all, delete-orphan"
+    )
+
+
+class Stat(db.Model):
+    __tablename__ = "stats"
+    id = db.Column(db.Integer, primary_key=True)
+    field = db.Column(db.String(10), nullable=False)
+    value = db.Column(db.Float, nullable=False)
+
+    hero_id = db.Column(db.Integer, db.ForeignKey("heroes.id"))
+
+
+class Ability(db.Model):
+    __tablename__ = "abilities"
+    id = db.Column(db.Integer, primary_key=True)
+    slot = db.Column(db.Integer, nullable=False)
+    level = db.Column(db.Integer, nullable=False, default=0)
+    enabled = db.Column(db.Boolean, nullable=False)
+
+    hero_id = db.Column(db.Integer, db.ForeignKey("heroes.id"))
