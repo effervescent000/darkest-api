dce6c9140aacf4db3414fad123d18103d6b92bfb
effervescent000 <kislio01@gmail.com>
2022-05-22 11:21:32 -0400

    added schema

---
+++ darkest_api/schema.py
@@ -0,0 +1,41 @@
+from . import ma
+
+
+class StatSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "field", "value", "hero_id")
+
+
+multi_stat_schema = StatSchema(many=True)
+
+
+class AbilitySchema(ma.Schema):
+    class Meta:
+        fields = ("id", "slot", "level", "enabled", "hero_id")
+
+
+multi_ability_schema = AbilitySchema(many=True)
+
+
+class HeroSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "hero_class", "resolve", "stats", "abilities")
+
+    stats = ma.Nested(multi_stat_schema)
+    abilities = ma.Nested(multi_ability_schema)
+
+
+multi_hero_schema = HeroSchema(many=True)
+
+
+class RosterSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "user_id", "heroes")
+
+    heroes = ma.Nested(multi_hero_schema)
+
+
+class UserSchema(ma.Schema):
+    # intentionally leaving off the roster right now
+    class Meta:
+        fields = ("id", "username", "role")
