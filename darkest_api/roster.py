a9c153a97264955cd4fe6b6bf4a075d0c0f9b41f
effervescent000 <kislio01@gmail.com>
2022-05-25 08:26:06 -0400

    added roster POST endpoint

--- darkest_api/roster.py
+++ darkest_api/roster.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request, jsonify, current_app
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, current_user
 
 from .models import Roster
 from .schema import RosterSchema
@@ -10,6 +11,18 @@ one_roster_schema = RosterSchema()
 # GET endpoints
 
 # POST endpoints
+
+
+@bp.route("/", methods=["POST"])
+@jwt_required()
+def add_roster():
+    data = request.get_json()
+    name = data.get("name")
+    roster = Roster(name=name, user_id=current_user.id)
+    db.session.add(roster)
+    db.session.commit()
+    return jsonify(one_roster_schema.dump(roster)), 201
+
 
 # PUT endpoints
 
