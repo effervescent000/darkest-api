48c8380a1d4907868e8f83509ea457d1ece6d668
effervescent000 <kislio01@gmail.com>
2022-05-27 09:59:44 -0400

    added test for roster POST endpoint

--- tests/test_roster.py
+++ tests/test_roster.py
@@ -1,8 +1,9 @@
-import pytest
+# GET endpoint tests
 
 
 def test_get_rosters(client, user_header):
     response = client.get("/roster/", headers=user_header)
+    assert response.status_code == 200
     data = response.json
     assert len(data) == 1
     assert data[0]["id"] == 2
@@ -10,3 +11,16 @@ def test_get_rosters(client, user_header):
     heroes = data[0]["heroes"]
     assert len(heroes) == 1
     assert heroes[0]["id"] == 2
+
+
+# POST endpoint tests
+
+
+def test_add_roster(client, user_header):
+    response = client.post(
+        "/roster/", json={"name": "A fake test roster"}, headers=user_header
+    )
+    assert response.status_code == 201
+    data = response.json
+    assert data["id"] == 3
+    assert len(data["heroes"]) == 0
