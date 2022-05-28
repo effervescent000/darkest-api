1edc1434a1d2ba8c5a2185863f2014feb20531ca
effervescent000 <kislio01@gmail.com>
2022-05-28 07:05:22 -0400

    added second test to hero POST test

--- tests/test_hero.py
+++ tests/test_hero.py
@@ -15,9 +15,18 @@ import tests.shapes as shapes
             shapes.hero_response_factory(id=3, hero_class="arbalest", roster_id=2),
             "Return a hero w/ a default name, w/o stats or abilities.",
         ),
+        (
+            shapes.hero_record_factory(
+                id=3, hero_class="crusader", name="Some Crusader"
+            ),
+            shapes.hero_response_factory(
+                id=3, hero_class="crusader", name="Some Crusader", roster_id=2
+            ),
+            "Return a hero with the correct name",
+        ),
     ],
 )
-def test_add_hero(client, user_header, given, expected, should):
+def test_add_hero_valid(client, user_header, given, expected, should):
     response = client.post("/hero/", json=given, headers=user_header)
     assert response.status_code == 201
 
